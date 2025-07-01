1449d4b478665e669c598ae9d80d306267cded65
Tim <timothyhye01@gmail.com>
2025-07-01 17:35:59 +0800

    feat: implemented category read(R) endpoints for MVP and corresponding schemas used

--- backend/routers/categories.py
+++ backend/routers/categories.py
@@ -1,11 +1,25 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
 from database import get_db
-from models import Category
+from schemas import categories
+import models
 
 router = APIRouter()
 
-@router.get("/")
-async def test():
-    return "this works perfectly!"
+@router.get("/", response_model = List[categories.CategoryResponse])
+async def get_categories(db: Session = Depends(get_db)):
+    categories = db.query(models.Category).all()
+    return categories
+    
+@router.get("/{category_id}", response_model = categories.CategoryResponse)
+async def get_category(category_id: int, db: Session = Depends(get_db)):
+    category = db.query(models.Category).filter(models.Category.id == category_id).first()
+    if not category:
+        raise HTTPException(status_code = 404, detail = "Category not found")
+    return category
+
+"""NOTE: Create, Update and Delete operations for categories will not be implemented in the mvp first iteration 
+as they are low priority features compared to the rest of the CRUD operations for auth and transactions as our 
+categories for now are seeded so we will only require these missing CRUD operations in later iterations when we
+start to allow for custom category creation, modification and deletion """
---
+++ backend/schemas/categories.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+
+# Both request and response schemas in the same file
+
+##///////////////////////////////////////
+##CategoryRequest will not be used in MVP
+##class CategoryRequest(BaseModel):
+    ##name: str = Field(..., min_length=1, max_length=50)
+
+    
+
+class CategoryResponse(BaseModel):
+    id: int
+    name: str
+    #created_at: datetime # Not needed yet unless for future use involving custom categories
+    
+    class Config:
+        from_attributes = True
