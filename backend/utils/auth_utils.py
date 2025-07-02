5762507bd82b3b869f0e703edc94f2bc801d3e67
Tim <timothyhye01@gmail.com>
2025-07-02 23:47:25 +0800

    fix: dealt with bug in auth_utils caused by error in imports

--- backend/utils/auth_utils.py
+++ backend/utils/auth_utils.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timezone, timedelta
 import os
 from dotenv import load_dotenv
 
@@ -25,9 +25,9 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 
 # JWT token handling functionality
-def create_access_token(data: dict, expires_delta: datetime.timedelta = None):
+def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
